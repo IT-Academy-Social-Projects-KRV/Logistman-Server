b6c6969c53d3cf41eb7917a943e16c4cd6cf88cd
Andrii <andrewchepeliuk@gmail.com>
2022-04-20 19:46:45 +0300

    all validation rules added

--- Core/Validation/UserRegistrationValidation.cs
+++ Core/Validation/UserRegistrationValidation.cs
@@ -23,27 +23,32 @@ namespace Core.Validation
                 .NotNull()
                 .NotEmpty()
                 .Length(2, 50)
-                .MustAsync(HasNumber).WithMessage("'{PropertyName}' must not contain numbers!");
+                .Matches(@"^\D*$").WithMessage("'{PropertyName}' must not contain digits!")
+                .Matches(@"\A[A-Z][a-z]{1,49}[a-z]*$").WithMessage("The first letter in '{PropertyName}' must be uppercase, the others must be lowercase!")
+                .Matches("^[^£# “”!@$%^&*(){}:;<>,.?/+_=|'~\\-]*$").WithMessage("'{PropertyName}' must not contain special characters!");
 
             RuleFor(user => user.Surname)
                 .NotNull()
                 .NotEmpty()
                 .Length(2, 50)
-                .MustAsync(HasNumber).WithMessage("'{PropertyName}' must not contain numbers!");
+                .Matches(@"^\D*$").WithMessage("'{PropertyName}' must not contain digits!")
+                .Matches(@"\A[A-Z][a-z]{1,49}[a-z]*$").WithMessage("The first letter in '{PropertyName}' must be uppercase, the others must be lowercase!")
+                .Matches("^[^£# “”!@$%^&*(){}:;<>,.?/+_=|'~\\-]*$").WithMessage("'{PropertyName}' must not contain special characters!");
+
 
             RuleFor(user => user.Email)
                 .NotNull()
                 .EmailAddress().WithMessage("'{PropertyValue}' - is not an email address!")
-                .MustAsync(IsUniqueUserEmail).WithMessage("A user with this email ({PropertyValue}) is already registered!");
+                .MustAsync(IsUniqueUserEmail).WithMessage("A user with this email ('{PropertyValue}') is already registered!");
 
             RuleFor(user => user.Password)
                 .NotNull()
                 .Length(8, 50)
-                .Matches("[A-Z]").WithMessage("'{PropertyName}' must contain one or more uppercase letters.")
-                .Matches("[a-z]").WithMessage("{PropertyName} must contain one or more lowercase letters.")
-                .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits.")
-                .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("'{PropertyName}' must contain one or more special characters.")
-                .Matches("^[^£# “”]*$").WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces.");
+                .Matches("[A-Z]").WithMessage("'{PropertyName}' must contain one or more uppercase letters!")
+                .Matches("[a-z]").WithMessage("'{PropertyName}' must contain one or more lowercase letters!")
+                .Matches(@"\d").WithMessage("'{PropertyName}' must contain one or more digits!")
+                .Matches(@"[][""!@$%^&*(){}:;<>,.?/+_=|'~\\-]").WithMessage("'{PropertyName}' must contain one or more special characters!")
+                .Matches("^[^£# “”]*$").WithMessage("'{PropertyName}' must not contain the following characters £ # “” or spaces!");
         }
 
         private async Task<bool> IsUniqueUserEmail(string email, CancellationToken cancellationToken)
@@ -51,9 +56,9 @@ namespace Core.Validation
             var userEntity = await _userManager.FindByEmailAsync(email);
             return userEntity == null;
         }
-        private async Task<bool> HasNumber(string val, CancellationToken cancellationToken)
-        {
-            return await Task.Run(() => val.Any(char.IsNumber) == false);
-        }
+        //private async Task<bool> HasNumber(string val, CancellationToken cancellationToken)
+        //{
+        //    return await Task.Run(() => val.Any(char.IsNumber) == false);
+        //}
     }
 }
