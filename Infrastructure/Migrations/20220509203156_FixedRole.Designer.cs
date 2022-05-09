95233a088175e9982175fcf39594794f47842530
Eugen <56602775+Eugene-pas@users.noreply.github.com>
2022-05-09 23:32:46 +0300

    Create an endpoint to add new offer for recipent/sender

--- Infrastructure/Migrations/20220509203156_FixedRole.Designer.cs
+++ Infrastructure/Migrations/20220509203156_FixedRole.Designer.cs
@@ -4,14 +4,16 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220509203156_FixedRole")]
+    partial class FixedRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
