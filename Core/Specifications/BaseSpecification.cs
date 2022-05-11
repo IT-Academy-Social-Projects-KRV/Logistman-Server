ea89e8a583b000c9a6d8fe14b6b51cbf0b020433
Grasmus <91031013+Grasmus@users.noreply.github.com>
2022-05-11 22:08:18 +0300

    Added query specifications

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,40 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications
+{
+    public class BaseSpecification<TEntity>: ISpecification<TEntity>
+    {
+        public BaseSpecification()
+        {
+
+        }
+
+        public BaseSpecification(Expression<Func<TEntity, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        public Expression<Func<TEntity, bool>> Criteria { get; }
+        public IList<Expression<Func<TEntity, object>>> Includes { get; } = new List<Expression<Func<TEntity, object>>>();
+        public Expression<Func<TEntity, object>> OrderBy { get; private set; }
+        public Expression<Func<TEntity, object>> OrderByDescending { get; private set; }
+
+        protected void AddInclude(Expression<Func<TEntity, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<TEntity, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+
+        protected void AddOrderByDescending(Expression<Func<TEntity, object>> orderByDescendingExpression)
+        {
+            OrderByDescending = orderByDescendingExpression;
+        }
+    }
+}
