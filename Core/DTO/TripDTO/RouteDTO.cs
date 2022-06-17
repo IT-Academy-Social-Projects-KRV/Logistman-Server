1e38116e79f6f13a9444592ae74c64ceb2c012a8
Grasmus <91031013+Grasmus@users.noreply.github.com>
2022-06-18 00:16:34 +0300

    Added distance field

--- Core/DTO/TripDTO/RouteDTO.cs
+++ Core/DTO/TripDTO/RouteDTO.cs
@@ -10,6 +10,7 @@ namespace Core.DTO.TripDTO
         public string Description { get; set; }
         public float LoadCapacity { get; set; }
         public int MaxRouteDeviationKm { get; set; }
+        public float Distance { get; set; }
         public UserFullNameDTO User { get; set; }
         public TripCarInfoDTO Car { get; set; }
         public List<PointDTO> Points { get; set; }
