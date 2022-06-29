9744b8dd46dedaaa2632284915e902ffd9d61b7f
Grasmus <91031013+Grasmus@users.noreply.github.com>
2022-06-29 15:38:02 +0300

    Added field isAnswered in invite preview DTO

--- Core/DTO/InviteDTO/InvitePreviewDTO.cs
+++ Core/DTO/InviteDTO/InvitePreviewDTO.cs
@@ -6,6 +6,7 @@ namespace Core.DTO.InviteDTO
     public class InvitePreviewDTO
     {
         public int Id { get; set; }
+        public bool IsAnswered { get; set; }
         public OfferPreviewForInviteDTO OfferInfo { get; set; }
         public TripPreviewForInviteDTO TripInfo { get; set; }
     }
